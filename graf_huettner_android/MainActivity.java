d9b3bf3fcf77e119e617abad01773db1447bbd5f
marlies-h <marlies.huettner@gmail.com>
2016-04-28 14:26:30 +0200

    MainActivity.java has been added

---
+++ graf_huettner_android/MainActivity.java
@@ -0,0 +1,17 @@
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		int numberOfOutputs;
+		
+		Out.print("Please enter the number of outputs: "); 
+		numberOfOutputs = In.readInt(); 
+		for (int i = 1; i <= numberOfOutputs; i++) {
+			Out.print("Output number ");
+			Out.print(i);
+			Out.println(": Hello World!");
+		}
+
+	}
+
+}
